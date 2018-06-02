0217e502b0f1bf6f241f43437802fdae211221bb
Bronn BlackWater <bblackwater9@gmail.com>
2018-06-02 19:32:16 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+const playlist = {}
+
+function updatePlaylist(object, artist, song) {
+    return object.artist = song;
+}
+
+function removeFromPlaylist(object,artist) {
+  delete object.artist;
+}
