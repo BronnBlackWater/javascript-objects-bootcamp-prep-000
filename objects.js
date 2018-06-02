7888edee526e2ec564f617ce8714a11fa68e673b
Bronn BlackWater <bblackwater9@gmail.com>
2018-06-02 19:40:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'The Rolling Stones': 'I Can\'t get No Satisfaction'}
 
 function updatePlaylist(playlist, artist, song) {
-    playlist.artist = song;
+    playlist[artist] = song;
     return playlist;
 }
 
